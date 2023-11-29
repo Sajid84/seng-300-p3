43deee390f24f7234b46e2e24079dc765a4c2fc7
bruh <cardboardboxpepe@gmail.com>
2023-11-28 21:57:58 -0700

    This doesn't work properly because Java. I honestly don't know why this doesn't work properly.

--- com.thelocalmarketplace.software/src/driver/SystemManagerForm.java
+++ com.thelocalmarketplace.software/src/driver/SystemManagerForm.java
@@ -1,19 +1,27 @@
 package driver;
 
 import com.jjjwelectronics.Item;
+import com.jjjwelectronics.scanner.Barcode;
 import com.jjjwelectronics.scanner.BarcodedItem;
 import com.jjjwelectronics.screen.ITouchScreen;
 import com.thelocalmarketplace.hardware.BarcodedProduct;
+import com.thelocalmarketplace.hardware.PLUCodedItem;
+import com.thelocalmarketplace.hardware.PLUCodedProduct;
+import com.thelocalmarketplace.hardware.Product;
 import managers.SystemManager;
 import managers.enums.ScanType;
 import managers.enums.SessionStatus;
 import managers.interfaces.IScreen;
 import utils.DatabaseHelper;
+import utils.Pair;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class SystemManagerForm implements IScreen {
 
@@ -40,20 +48,30 @@ public class SystemManagerForm implements IScreen {
     private JButton testEnableMachineButton;
     private JButton testDisableMachineButton;
     private JButton addTooHeavyItemButton;
+    private JCheckBox doNotBagItemCheckBox;
+    private JButton exitSessionButton;
 
     public SystemManagerForm(SystemManager sm) {
         // copying the system manager reference
         this.sm = sm;
 
         // setup the table
-        DefaultTableModel model = new DefaultTableModel();
-        model.addColumn("Name");
-        model.addColumn("Price");
+        DefaultTableModel model = new DefaultTableModel(new Object[]{
+                "Name",
+                "Price",
+                "Bagged?"
+        }, 0);
 
         // setting the model
         itemsTable.setModel(model);
 
-        // listener
+        // setting the label
+        updateFeedbackLabel();
+
+        // setting the state of the remove item button
+        updateRemoveItemButtonState();
+
+        // events
         scanByMainScannerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -145,6 +163,42 @@ public class SystemManagerForm implements IScreen {
                 sm.signalForAttendant();
             }
         });
+        searchForItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Searching for item was initiated");
+            }
+        });
+        doNotBagItemCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Do not bag item was triggered: " + e.getActionCommand());
+                sm.doNotBagRequest(!doNotBagItemCheckBox.isSelected());
+            }
+        });
+        removeItemButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                /**
+                 * Originally, I wanted to implement something where each row would have a checkbox and for
+                 * each box that was checked, this function would remove it.
+                 *
+                 * However, because Java Swing doesn't natively support rendering checkboxes, so I can't do my nice
+                 * solution. WHY ISN'T THIS SUPPORTED NATIVELY?????????
+                 *
+                 * This is my solution, too bad!
+                 */
+                sm.removeItemFromOrder(sm.getItems().get(sm.getItems().size() - 1));
+            }
+        });
+    }
+
+    /**
+     * This function updates the enabled state of the button based on the remove
+     * item check boxes.
+     */
+    protected void updateRemoveItemButtonState() {
+        removeItemButton.setEnabled(!sm.getItems().isEmpty());
     }
 
     @Override
@@ -157,17 +211,43 @@ public class SystemManagerForm implements IScreen {
         touchScreen.getFrame().setContentPane(root);
     }
 
-    protected void updateTable(Item item, boolean append) {
+    /**
+     * TODO this function is subject to be redone
+     *
+     * @param item
+     * @param append
+     */
+    protected void updateTable() {
         // getting the model
         DefaultTableModel model = (DefaultTableModel) itemsTable.getModel();
 
-        // adding the item to the table
-        if (append) {
-            // getting the product
+        // clearing the table
+        for (int i = 0; i < model.getRowCount(); ++i) {
+            model.removeRow(i);
+        }
+
+        // updating the table
+        for (Pair<Item, Boolean> pair : sm.getItems()) {
+            Item item = pair.getKey();
+
+            // WHY DOESN'T PRODUCT HAVE A GET DESCRIPTION FUNCTION
+            String description = "You shouldn't see this";
+            double price = 0;
+
+            // getting the product information
             if (item instanceof BarcodedItem) {
                 BarcodedProduct prod = DatabaseHelper.get((BarcodedItem) item);
-                model.addRow(new Object[]{prod.getDescription(), "$ " + prod.getPrice()});
+                description = prod.getDescription();
+                price = prod.getPrice();
             }
+            if (item instanceof PLUCodedItem) {
+                PLUCodedProduct prod = DatabaseHelper.get((PLUCodedItem) item);
+                description = prod.getDescription();
+                price = prod.getPrice();
+            }
+
+            // adding the row
+            model.addRow(new Object[]{ description, "$ " + price, pair.getValue() });
         }
 
         // updating the label
@@ -180,22 +260,35 @@ public class SystemManagerForm implements IScreen {
 
     @Override
     public void notifyItemAdded(Item item) {
-        updateTable(item, true);
+        // updating the table
+        updateTable();
+
+        // clearing the do not bag request
+        doNotBagItemCheckBox.setSelected(false);
+
+        // updating the state of the remove item button
+        updateRemoveItemButtonState();
     }
 
     @Override
     public void notifyItemRemoved(Item item) {
-        updateTable(item, false);
+        // updating the table
+        updateTable();
+
+        // updating the state of the remove item button
+        updateRemoveItemButtonState();
     }
 
     @Override
     public void notifyStateChange(SessionStatus state) {
         switch (state) {
             case NORMAL -> {
+                updateFeedbackLabel();
                 unblockButtons();
             }
             case BLOCKED -> {
                 blockButtons();
+                determineCause();
             }
             case PAID -> {
                 // TODO do something different here?
@@ -205,6 +298,44 @@ public class SystemManagerForm implements IScreen {
                 // TODO implement the disabled state
             }
         }
+    }
+
+    protected void determineCause() {
+        if (sm.errorAddingItem()) {
+            updateFeedbackLabel("There was an error adding an item to the cart, please re-scan the item.");
+        }
+
+        if (sm.isScaleOverloaded()) {
+            updateFeedbackLabel("The item you placed in the bagging area was too heavy, please do not bag this item.");
+        }
+    }
+
+    /**
+     * This is to quickly set the feedback label to nothing,
+     * essentially clearing the label.
+     */
+    protected void updateFeedbackLabel() {
+        updateFeedbackLabel(null);
+    }
+
+    /**
+     * This updates the feedback label to the desired text in the color RED.
+     * This should only be used to indicate some form of error that the system encountered.
+     *
+     * @param message the message to be displayed
+     */
+    protected void updateFeedbackLabel(String message) {
+        System.out.println("Cause: " + message);
+        feedbackLabel.setForeground(Color.RED);
+
+        // switching based on message
+        if ((message == null) || (message.isEmpty())) {
+            feedbackLabel.setText("");
+            return;
+        }
+
+        // updating label
+        feedbackLabel.setText("BLOCKED: " + message);
     }
 
     protected void blockButtons() {
