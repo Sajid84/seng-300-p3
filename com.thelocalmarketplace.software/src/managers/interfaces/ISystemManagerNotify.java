62029a2150bc9c0a6ae3d6d8fa763e9d17155912
bruh <cardboardboxpepe@gmail.com>
2023-11-29 20:51:41 -0700

    added comments

--- com.thelocalmarketplace.software/src/managers/interfaces/ISystemManagerNotify.java
+++ com.thelocalmarketplace.software/src/managers/interfaces/ISystemManagerNotify.java
@@ -3,12 +3,29 @@ package managers.interfaces;
 import com.jjjwelectronics.Item;
 import managers.enums.SessionStatus;
 
+import java.math.BigDecimal;
+
 public interface ISystemManagerNotify {
 
+    /**
+     * This notifies all listeners that an item was added to the order.
+     *
+     * @param item the item added
+     */
     void notifyItemAdded(Item item);
 
+    /**
+     * This notifies all listeners that an item was removed from the order.
+     *
+     * @param item the item removed
+     */
     void notifyItemRemoved(Item item);
 
+    /**
+     * This notifies all listeners that the state of the system manager has changed.
+     *
+     * @param state the new state of the system manager
+     */
     void notifyStateChange(SessionStatus state);
 
 }
