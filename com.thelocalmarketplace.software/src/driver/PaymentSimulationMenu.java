897bd8e50ecb62fa02ee6b0a2cdbb165a73cd5dc
Logan Miszaniec <lmiszaniec@gmail.com>
2023-11-29 19:53:12 -0700

    Updated payment simulator GUI

--- com.thelocalmarketplace.software/src/driver/PaymentSimulationMenu.java
+++ com.thelocalmarketplace.software/src/driver/PaymentSimulationMenu.java
@@ -1,6 +1,9 @@
 package driver;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 import javax.swing.*;
 
@@ -14,29 +17,101 @@ public class PaymentSimulationMenu {
 	private PaymentManager pm;
 	private Card SimulatedCard;
 	
-	private JFrame PaymentWindow;
+	private JFrame PaymentWindow = new JFrame("Payment Simulation");
+	private JPanel ButtonPane = new JPanel();
+		
+	private JPanel CardPanel = new JPanel();
+	private JButton SwipeCardButton = new JButton("Swipe Card");
+	private JButton TapCardButton = new JButton("Tap Card");
+	private JButton InsertCardButton = new JButton("Insert Card");
 	
-	private JPanel CardPanel;
-	private JButton SwipeCardButton;
-	private JButton TapCardButton;
-	
-	private JPanel CoinCashPanel;
-	private JButton InsertCoinButton;
-	private JButton InsertBanknoteButton;
+	private JPanel CoinCashPanel = new JPanel();
+	private JButton InsertCoinButton = new JButton("Insert Coin");
+	private JButton InsertBanknoteButton = new JButton("Insert Banknote");
 	
 	public PaymentSimulationMenu(PaymentManager pm) {
 		this.pm = pm;
 		SimulatedCard = CardHelper.createCard(CardHelper.createCardIssuer());
 		buildWindow();
+		setupEvents();
+		PaymentWindow.setSize(200, 400);
+		PaymentWindow.setVisible(true);
 		
 	}
 	
 	private void buildWindow() {
-		PaymentWindow = new JFrame();
-		PaymentWindow.setLayout(new GridLayout(1,2));
-		CardPanel = new JPanel();
-		SwipeCardButton = new JButton("Swipe Card");
-		TapCardButton = new JButton("Tap Card");
+		CardPanel.setLayout(new BoxLayout(CardPanel, BoxLayout.Y_AXIS));
+		CardPanel.add(SwipeCardButton);
+		CardPanel.add(Box.createRigidArea(new Dimension(0,5)));
+		CardPanel.add(TapCardButton);
+		CardPanel.add(Box.createRigidArea(new Dimension(0,5)));
+		CardPanel.add(InsertCardButton);
 		
+		CoinCashPanel.setLayout(new BoxLayout(CoinCashPanel, BoxLayout.Y_AXIS));
+		CoinCashPanel.add(InsertCoinButton);
+		CoinCashPanel.add(Box.createRigidArea(new Dimension(0,5)));
+		CoinCashPanel.add(InsertBanknoteButton);
+		
+		ButtonPane.setLayout(new BoxLayout(ButtonPane, BoxLayout.X_AXIS));
+		ButtonPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		ButtonPane.add(CoinCashPanel);
+		ButtonPane.add(Box.createRigidArea(new Dimension(5,0)));
+		ButtonPane.add(CardPanel);
+		
+		PaymentWindow.add(ButtonPane);
+		
+	}
+	
+	private void setupEvents() {
+		SwipeCardButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					pm.swipeCard(SimulatedCard);
+				} catch (IOException e1) {
+					// shouldn't happen
+				}
+				
+			}
+			
+		});
+		TapCardButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//pm.tapCard(SimulatedCard); // TODO: update when implemented
+				
+			}
+			
+		});
+		InsertCardButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//pm.insertCard(SimulatedCard); // TODO: update when implemented
+				
+			}
+			
+		});
+		
+		InsertCoinButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pm.insertCoin(null); // TODO: put an actual coin here
+				
+			}
+			
+		});
+		InsertBanknoteButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				pm.insertBanknote(null); // TODO: put an actual banknote here
+				
+			}
+			
+		});
 	}
 }
