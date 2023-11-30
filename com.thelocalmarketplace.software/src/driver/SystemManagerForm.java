08091566e7c85bdb50ac73f81e1aab7620e40f5a
bruh <cardboardboxpepe@gmail.com>
2023-11-29 20:55:03 -0700

    did more work on the GUI refactored the debug window out of the main view

--- com.thelocalmarketplace.software/src/driver/SystemManagerForm.java
+++ com.thelocalmarketplace.software/src/driver/SystemManagerForm.java
@@ -1,3 +1,5 @@
+// Liam Major 30223023
+
 package driver;
 
 import com.jjjwelectronics.Item;
@@ -20,6 +22,7 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,14 +45,9 @@ public class SystemManagerForm implements IScreen {
     private JButton removeItemButton;
     private JButton addOwnBagsButton;
     private JButton purchaseBagsButton;
-    private JButton causeWeightDiscrepancyButton;
-    private JButton blockSessionButtonButton;
-    private JButton unblockSessionButtonButton;
-    private JButton testEnableMachineButton;
-    private JButton testDisableMachineButton;
-    private JButton addTooHeavyItemButton;
     private JCheckBox doNotBagItemCheckBox;
     private JButton exitSessionButton;
+    private DebugForm debug;
 
     // TABLE HEADERS
     private final String nameColumn = "Name";
@@ -66,6 +64,11 @@ public class SystemManagerForm implements IScreen {
     public SystemManagerForm(SystemManager sm) {
         // copying the system manager reference
         this.sm = sm;
+        this.debug = new DebugForm(sm);
+
+        // attaching to the observer
+        sm.attach(this);
+        sm.attach(debug);
 
         // setting the model
         itemsTable.setModel(generateModelSkeleton());
@@ -89,7 +92,6 @@ public class SystemManagerForm implements IScreen {
                 sm.addItemToOrder(item, ScanType.MAIN);
             }
         });
-
         scanByHandheldScannerButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -109,58 +111,7 @@ public class SystemManagerForm implements IScreen {
                 sm.addCustomerBags(DatabaseHelper.createCustomerBags());
             }
         });
-        purchaseBagsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO purchase bags use case
-            }
-        });
-        payForOrderButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO do something here to actually pay for the order
-            }
-        });
-        causeWeightDiscrepancyButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Causing a weight discrepancy.");
-            }
-        });
-        testEnableMachineButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Requesting the machine to be enabled.");
-                sm.requestEnableMachine();
-            }
-        });
-        blockSessionButtonButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Blocking the session.");
-                sm.blockSession();
-            }
-        });
-        unblockSessionButtonButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Using attendant override to unblock the session.");
-                sm.onAttendantOverride();
-            }
-        });
-        testDisableMachineButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Requesting the machine to be disabled.");
-                sm.requestDisableMachine();
-            }
-        });
-        addTooHeavyItemButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.out.println("Overloading the scales with an item.");
-            }
-        });
+
         signalForAttendantButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -235,6 +186,11 @@ public class SystemManagerForm implements IScreen {
 
     @Override
     public void configure(ITouchScreen touchScreen) {
+        // attaching debug window to the correct panel
+        debugView.setLayout(new GridLayout());
+        debugView.add(debug.getPanel());
+
+        // updating the touch screen
         touchScreen.getFrame().setContentPane(root);
     }
 
@@ -331,6 +287,17 @@ public class SystemManagerForm implements IScreen {
                 // TODO implement the disabled state
             }
         }
+    }
+
+    @Override
+    public void notifyRefresh() {
+        updateTable();
+        updateRemoveItemButtonState();
+    }
+
+    @Override
+    public void notifyPaymentAdded(BigDecimal value) {
+        // TODO do something with this
     }
 
     protected void determineCause() {
