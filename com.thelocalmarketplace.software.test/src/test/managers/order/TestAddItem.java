11961988bdb5b281cea8276f51398cf83053ee31
Abdullah Ishtiaq <ishtiaqabdullah11@gmail.com>
2023-12-01 01:15:42 -0700

    created test cases for additembytextsearch

--- com.thelocalmarketplace.software.test/src/test/managers/order/TestAddItem.java
+++ com.thelocalmarketplace.software.test/src/test/managers/order/TestAddItem.java
@@ -3,6 +3,7 @@
 package test.managers.order;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 import java.math.BigDecimal;
 
@@ -12,19 +13,23 @@ import com.thelocalmarketplace.hardware.ISelfCheckoutStation;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jjjwelectronics.Item;
 import com.jjjwelectronics.Mass;
 import com.jjjwelectronics.scanner.BarcodedItem;
 import com.thelocalmarketplace.hardware.AbstractSelfCheckoutStation;
 import com.thelocalmarketplace.hardware.BarcodedProduct;
 import com.thelocalmarketplace.hardware.PLUCodedItem;
+import com.thelocalmarketplace.hardware.PLUCodedProduct;
 import com.thelocalmarketplace.hardware.external.ProductDatabases;
 
+import database.Database;
 import managers.enums.ScanType;
 import stubbing.StubbedBarcodedProduct;
 import stubbing.StubbedGrid;
 import stubbing.StubbedItem;
 import stubbing.StubbedOrderManager;
 import stubbing.StubbedPLUItem;
+import stubbing.StubbedPLUProduct;
 import stubbing.StubbedStation;
 import stubbing.StubbedSystemManager;
 
@@ -97,4 +102,50 @@ public class TestAddItem {
 		PLUCodedItem pluCodedItem = new StubbedPLUItem();
 		om.addItemToOrder(pluCodedItem, ScanType.HANDHELD);
 	}
+	
+	@Test
+    public void searchItemsByText_ValidDescription_ReturnsBarcodedItem() {
+		
+        BarcodedProduct prod = new StubbedBarcodedProduct();
+        ProductDatabases.BARCODED_PRODUCT_DATABASE.put(prod.getBarcode(), prod);
+
+        BarcodedItem expectedItem = new BarcodedItem(prod.getBarcode(), new Mass(prod.getExpectedWeight()));
+
+        Item result = om.searchItemsByText(prod.getDescription());
+
+        assertEquals(expectedItem, result);
+    }
+
+    @Test
+    public void searchItemsByText_InvalidDescription_ReturnsNull() {
+    	
+        BarcodedProduct prod = new StubbedBarcodedProduct();
+        ProductDatabases.BARCODED_PRODUCT_DATABASE.put(prod.getBarcode(), prod);
+
+        Item result = om.searchItemsByText("Invalid Description");
+
+        assertNull(result);
+    }
+
+    @Test
+    public void searchItemsByText_PLUProduct_ReturnsPLUCodedItem() {
+    	
+        PLUCodedProduct pluProduct = new StubbedPLUProduct();
+        Database.PLU_PRODUCT_DATABASE.put(pluProduct.getPLUCode(), pluProduct);
+
+        PLUCodedItem expectedItem = new StubbedPLUItem();
+        Database.PLU_ITEM_DATABASE.put(pluProduct.getPLUCode(), expectedItem);
+
+        Item result = om.searchItemsByText(pluProduct.getDescription());
+
+        assertEquals(expectedItem, result);
+    }
+
+    @Test
+    public void searchItemsByText_NullDescription_ReturnsNull() {
+    	
+        Item result = om.searchItemsByText(null);
+
+        assertNull(result);
+    }
 }
