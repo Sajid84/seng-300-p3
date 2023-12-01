e925a7d8daab568141248cd73dfa7661dec1034a
falahsheikh <112839415+falahsheikh@users.noreply.github.com>
2023-11-30 21:42:03 -0700

    work env

---
+++ com.thelocalmarketplace.software.test/src/test/managers/attendent/com/thelocalmarketplace/software/src/managers/MaintainInkTest.java
@@ -0,0 +1,14 @@
+// Sheikh Falah Sheikh Hasan - 30175335
+
+package com.thelocalmarketplace.software.src.managers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MaintainInkTest {
+
+    @Test
+    public void maintainInk() {
+    }
+}
