a2d087e501de5a095cd01a7420c31e402944f81b
Logan Miszaniec <lmiszaniec@gmail.com>
2023-12-01 18:18:51 -0700

    preliminary implementation of button actions

--- com.thelocalmarketplace.software/src/driver/AttendantViewGUI.java
+++ com.thelocalmarketplace.software/src/driver/AttendantViewGUI.java
@@ -3,6 +3,8 @@ package driver;
 import javax.swing.JPanel;
 import javax.swing.BoxLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.math.BigDecimal;
 
 import javax.swing.border.EmptyBorder;
@@ -11,6 +13,7 @@ import com.jjjwelectronics.Item;
 import com.jjjwelectronics.card.Card;
 import com.jjjwelectronics.screen.ITouchScreen;
 
+import managers.SystemManager;
 import managers.enums.SessionStatus;
 import managers.interfaces.IScreen;
 
@@ -24,6 +27,8 @@ import java.awt.Color;
 public class AttendantViewGUI extends JPanel implements IScreen{
 
 	private static final long serialVersionUID = 1L;
+	private SystemManager sm;
+	private JButton FillInkButton;
 	private JButton EnableMachineButton;
 	private JButton FillPaperButton;
 	private JButton DisableMachineButton;
@@ -38,7 +43,9 @@ public class AttendantViewGUI extends JPanel implements IScreen{
 	/**
 	 * Create the panel.
 	 */
-	public AttendantViewGUI() {
+	public AttendantViewGUI(SystemManager sm) {
+		this.sm = sm;
+		
 		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
 		
 		EventViewerPanel = new JPanel();
@@ -53,36 +60,138 @@ public class AttendantViewGUI extends JPanel implements IScreen{
 		add(ButtonPanel);
 		ButtonPanel.setLayout(new GridLayout(5, 2, 5, 5));
 		
-		JButton FillInkButton = new JButton("Fill Ink");
+		FillInkButton = new JButton("Fill Ink");
+		FillInkButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.maintainInk();
+				
+			}
+			
+		});
 		ButtonPanel.add(FillInkButton);
 		
 		EnableMachineButton = new JButton("Enable Machine");
+		EnableMachineButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.requestEnableMachine();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(EnableMachineButton);
 		
-		FillPaperButton = new JButton("FIll Paper");
+		FillPaperButton = new JButton("Fill Paper");
+		FillPaperButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.maintainPaper();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(FillPaperButton);
 		
 		DisableMachineButton = new JButton("Disable Machine");
+		DisableMachineButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.requestDisableMachine();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(DisableMachineButton);
 		
 		FillCoinsButton = new JButton("Fill Coins");
+		FillCoinsButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.maintainCoinDispensers();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(FillCoinsButton);
 		
 		UnloadCoinsButton = new JButton("Unload Coins");
+		UnloadCoinsButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.maintainCoinStorage();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(UnloadCoinsButton);
 		
 		FillBanknotesButton = new JButton("Fill Banknotes");
+		FillBanknotesButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.maintainBanknoteDispensers();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(FillBanknotesButton);
 		
 		UnloadBanknotesButton = new JButton("Unload Banknotes");
+		UnloadBanknotesButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.maintainBanknoteStorage();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(UnloadBanknotesButton);
 		
 		UnblockSessionButton = new JButton("Unblock Session");
+		UnblockSessionButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				sm.unblockSession();
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		ButtonPanel.add(UnblockSessionButton);
 		
 		ExtraActionButton = new JButton("EXTRA");
+		ExtraActionButton.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO or not TODO, that is the question (yes if we need an extra thing to do)
+				
+			}
+			
+		});
 		ButtonPanel.add(ExtraActionButton);
 
+	}
+	
+	public void AddEvent(String eventName) {
+		
 	}
 
 	@Override
