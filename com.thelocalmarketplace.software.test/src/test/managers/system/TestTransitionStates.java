ed01545ae3de212015f7f840db2fb26e2514fa9d
maleeha739 <149423210+maleeha739@users.noreply.github.com>
2023-12-02 19:02:55 -0700

    Update TestTransitionStates.java
    added comments

--- com.thelocalmarketplace.software.test/src/test/managers/system/TestTransitionStates.java
+++ com.thelocalmarketplace.software.test/src/test/managers/system/TestTransitionStates.java
@@ -24,12 +24,13 @@ public class TestTransitionStates {
 		sm = new StubbedSystemManager();
 	}
 	
+	//test case where IllegalArgumentException will be expected for when test cannot set a null state
 	@Test(expected = IllegalArgumentException.class)
 	public void testCannotSetANullState() {
 		sm.setState(null);
 	}
 
-
+	//test case for when blocked session will go from normal to blocked state
 	@Test
 	public void testBlockSession() {
 		sm.setState(SessionStatus.NORMAL);
@@ -39,6 +40,7 @@ public class TestTransitionStates {
 		assertEquals(sm.getState(), SessionStatus.BLOCKED);
 	}
 
+	//test case for when unblocked session will go from blocked to normal state
 	@Test
 	public void testUnblockSession() {
 		sm.setState(SessionStatus.BLOCKED);
@@ -48,6 +50,7 @@ public class TestTransitionStates {
 		assertEquals(sm.getState(), SessionStatus.NORMAL);
 	}
 
+	//test case for when unblocked session goes from paid state and is expected as RuntimeException 
 	@Test(expected = RuntimeException.class)
 	public void testCannotUnblockSessionFromPaid() {
 		sm.setState(SessionStatus.PAID);
@@ -55,6 +58,7 @@ public class TestTransitionStates {
 		sm.unblockSession();
 	}
 
+	//test case for when notifyPaid session will go from normal to paid state
 	@Test
 	public void testNotifyPaid() {
 		sm.setState(SessionStatus.NORMAL);
@@ -64,6 +68,7 @@ public class TestTransitionStates {
 		assertEquals(sm.getState(), SessionStatus.PAID);
 	}
 
+	//test case for when notifyPaid goes from blocked state and is expect as RuntimeException
 	@Test(expected = RuntimeException.class)
 	public void testNotifyPaidFromBlocked() {
 		sm.setState(SessionStatus.BLOCKED);
