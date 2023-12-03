2ea3048338ac6788d16f5c321b249dac11eda33d
maleeha739 <149423210+maleeha739@users.noreply.github.com>
2023-12-02 18:59:59 -0700

    Update TestBlockedFunctions.java
    added comments

--- com.thelocalmarketplace.software.test/src/test/managers/system/TestBlockedFunctions.java
+++ com.thelocalmarketplace.software.test/src/test/managers/system/TestBlockedFunctions.java
@@ -28,6 +28,7 @@ public class TestBlockedFunctions {
 		sm = new StubbedSystemManager();
 	}
 	
+	//test case for when card is swiped when session is blocked
 	@Test(expected = IllegalStateException.class)
 	public void testSwipeCardWhenBlocked() throws IOException {
 		sm.setState(SessionStatus.BLOCKED);
@@ -35,13 +36,15 @@ public class TestBlockedFunctions {
 		sm.swipeCard(null);
 	}
 
+	//test case for when card is swiped when session in a paid state
 	@Test(expected = IllegalStateException.class)
 	public void testSwipeCardWhenPaid() throws IOException {
 		sm.setState(SessionStatus.PAID);
 
 		sm.swipeCard(null);
 	}
-
+	
+	//test case for when you cannot insert a coin because session is in a blocked state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotInsertCoinWhenBlocked() {
 		sm.setState(SessionStatus.BLOCKED);
@@ -49,6 +52,7 @@ public class TestBlockedFunctions {
 		sm.insertCoin(null);
 	}
 
+	//test case for when you cannot insert a coin when session is in a paid state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotInsertCoinWhenPaid() {
 		sm.setState(SessionStatus.PAID);
@@ -56,6 +60,7 @@ public class TestBlockedFunctions {
 		sm.insertCoin(null);
 	}
 
+	//test case for when you cannot insert a banknote when session is in a blocked state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotInsertBanknoteWhenBlocked() {
 		sm.setState(SessionStatus.BLOCKED);
@@ -63,6 +68,7 @@ public class TestBlockedFunctions {
 		sm.insertBanknote(null);
 	}
 
+	//test case for when you cannot insert a banknote when session is in paid state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotInsertBanknoteWhenPaid() {
 		sm.setState(SessionStatus.PAID);
@@ -70,6 +76,7 @@ public class TestBlockedFunctions {
 		sm.insertBanknote(null);
 	}
 
+	//test case for when you cannot tender change when session is in blocked state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotTenderChangeWhenBlocked() throws RuntimeException, NoCashAvailableException {
 		sm.setState(SessionStatus.BLOCKED);
@@ -77,6 +84,7 @@ public class TestBlockedFunctions {
 		sm.tenderChange();
 	}
 
+	//test case for when you cannot tender change when session is in a normal state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotTenderChangeWhenNormal() throws RuntimeException, NoCashAvailableException {
 		sm.setState(SessionStatus.NORMAL);
@@ -84,6 +92,7 @@ public class TestBlockedFunctions {
 		sm.tenderChange();
 	}
 
+	//test case for when you cannot add an item when session is in a blocked state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotAddItemWhenBlocked() throws OperationNotSupportedException {
 		sm.setState(SessionStatus.BLOCKED);
@@ -91,6 +100,7 @@ public class TestBlockedFunctions {
 		sm.addItemToOrder(null, null);
 	}
 
+	//test case for when you cannot add an item when session is in paid state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotAddItemWhenPaid() throws OperationNotSupportedException {
 		sm.setState(SessionStatus.PAID);
@@ -98,13 +108,15 @@ public class TestBlockedFunctions {
 		sm.addItemToOrder(null, null);
 	}
 
+	//test case for when you cannot record a transaction when session is in blocked state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotRecordTransactionFromBlocked() {
 		sm.setState(SessionStatus.BLOCKED);
 
 		sm.recordTransaction(null, null, null);
 	}
-
+	
+	//test case for when you cannot record transaction when session is in paid state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotRecordTransactionFromPaid() {
 		sm.setState(SessionStatus.PAID);
@@ -112,6 +124,7 @@ public class TestBlockedFunctions {
 		sm.recordTransaction(null, null, null);
 	}
 	
+	//test case for when you cannot add bags when session is in blocked state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotAddBagsWhenBlocked() {
 		sm.setState(SessionStatus.BLOCKED);
@@ -119,6 +132,7 @@ public class TestBlockedFunctions {
 		sm.addCustomerBags(null);
 	}
 
+	//test case for when you cannot add bags when session is in paid state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotAddBagsWhenPaid() {
 		sm.setState(SessionStatus.PAID);
@@ -126,6 +140,7 @@ public class TestBlockedFunctions {
 		sm.addCustomerBags(null);
 	}
 	
+	//test case for when the attendant cannot override when session is in paid state
 	@Test(expected = IllegalStateException.class)
 	public void testAttendantCannotOverrideWhenPaid() {
 		sm.setState(SessionStatus.PAID);
@@ -133,6 +148,7 @@ public class TestBlockedFunctions {
 		sm.onAttendantOverride();
 	}
 	
+	//test case for when you cannot remove an item when session is in paid state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotRemoveItemWhenPaid() throws OperationNotSupportedException {
 		sm.setState(SessionStatus.PAID);
@@ -140,6 +156,7 @@ public class TestBlockedFunctions {
 		sm.removeItemFromOrder(null);
 	}
 	
+	//test case for when you cannot print receipt when session is in blocked state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotPrintReceiptWhenBlocked() {
 		sm.setState(SessionStatus.BLOCKED);
@@ -147,6 +164,7 @@ public class TestBlockedFunctions {
 		sm.printReceipt(null, null);
 	}
 	
+	//test case for when you cannot print receipt when session is in normal state
 	@Test(expected = IllegalStateException.class)
 	public void testCannotPrintReceiptWhenNormal() {
 		sm.setState(SessionStatus.NORMAL);
